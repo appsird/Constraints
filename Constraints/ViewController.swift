0cd4ff4a18d9784c00602d991d56f14b37bcb310
appsird <brian.balthazor@gmail.com>
2021-07-12 20:35:59 -0700

    Initial Commit

---
+++ Constraints/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Constraints
+//
+//  Created by Brian on 7/12/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
